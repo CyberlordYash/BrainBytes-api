4b6adf085b1331cefb905a4132257b64557eafd9
Yash Sachan <yashsachan321@gmail.com>
2024-03-10 14:02:14 +0530

    modified cors

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 //middlewares
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({ credentials: true, origin: "https://brainbytes-yash.netlify.app/" })
+);
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use("/api/users", userRoutes);
